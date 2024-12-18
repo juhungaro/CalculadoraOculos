37dc9e939518f5fbf7b6ba15f54b3ff1d2b47f3c
Juliana Hungaro <61634756+juhungaro@users.noreply.github.com>
2024-12-18 19:08:42 -0300

    Update Calculadora_oculos.py
    inclusão do logo

--- Calculadora_oculos.py
+++ Calculadora_oculos.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+from PIL import Image
+
+# Carregar e exibir o logo
+def mostrar_logo():
+    logo = Image.open("logo_clinica.png")
+    st.image(logo, width=300)  
 
 def calcular_economia(custo_anual_oculos, custo_caixa_lentes, caixas_por_ano, custo_frasco_solucao, frascos_por_ano, custo_cirurgia, anos_futuro):
     custos_anuais = []
